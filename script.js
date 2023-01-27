5205404583dde175e514decba4d240dda37f00b7
Fortwin <basirathamz@gmail.com>
2023-01-27 17:12:44 +0100

    Interactions

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+// function myFunction(){
+//     var shakes = document.getElementById('shakes')
+// }
+
+
+
+const breakfastId = document.getElementById('breakfast')
+const lunchId = document.getElementById('.lunch')
+const shakesId = document.getElementById('shakes')
+
+const breakfastBtn = document.getElementById('breakfastId')
+const lunchBtn = document.getElementById('lunchId')
+const shakesBtn = document.getElementById('shakesId')
+
+shakesBtn.addEventListener('click', function(){
+    breakfastId.style.display ='none';
+    lunchId.style.display = 'none';
+})
+
+
+
+
+
+// breakfastBtn.addEventListener('click', function(){
+//     shakesId.style.display= "none";
+//     lunchId.style.display= "none";
+// })
+// breakfastBtn.addEventListener('click', function(){
+//     // shakesId.style.display= "none";
+//     lunchId.style.display= "none";
+// })
+
+
+
+
+
+
+
+
+
+// const butonnnId = document.getElementById('herBtn')
+// const carrdId = document.getElementById('poland')
+
+// butonnnId.addEventListener('click', function(){
+//      carrdId.style.display ="none";
+// })
+;;;;;;;;;
