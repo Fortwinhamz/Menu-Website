f973e71432dcc0f85863029e53cde61598f8df08
Fortwin <basirathamz@gmail.com>
2023-01-28 19:43:06 +0100

    Interactions

--- script.js
+++ script.js
@@ -6,19 +6,46 @@
 // }
 
 
-
-const breakfastId = document.getElementById('breakfast')
-const lunchId = document.getElementById('.lunch')
-const shakesId = document.getElementById('shakes')
-
-const breakfastBtn = document.getElementById('breakfastId')
-const lunchBtn = document.getElementById('lunchId')
-const shakesBtn = document.getElementById('shakesId')
-
-shakesBtn.addEventListener('click', function(){
-    breakfastId.style.display ='none';
-    lunchId.style.display = 'none';
+document.querySelectorAll('.js-menu-filter-btn').forEach(function(btn){
+    btn.addEventListener('click', function(){
+        const filterStr = btn.getAttribute('data-menu-filter');
+        const containerId = btn.getAttribute('data-container-id');
+        filterMenuCards(filterStr, containerId)
+    })
 })
+
+function filterMenuCards(filterString, containerId){
+
+    const container = document.getElementById(containerId)
+
+    if(filterString === 'all'){
+        container.querySelectorAll('.menu-box').forEach(function(menu){
+            menu.removeAttribute('style')
+        })
+    } else {
+        container.querySelectorAll('.menu-box').forEach(function(menu){
+            if(!menu.classList.contains(filterString)){
+                menu.style.display = 'none';
+            } else {
+                menu.removeAttribute('style')
+            }
+        })
+    }
+}
+// const breakfastId = document.getElementById('breakfast')
+// const lunchId = document.getElementById('.lunch')
+// const shakesId = document.getElementById('shakes')
+
+// const breakfastBtn = document.getElementById('breakfastId')
+// const lunchBtn = document.getElementById('lunchId')
+// const shakesBtn = document.getElementById('shakesId')
+
+// console.log(shakesBtn)
+// shakesBtn.addEventListener('click', function(){
+//     document.querySelectorAll('.js-breakfast-opt').forEach(option => {
+//         option.style.display = 'none'
+//     })
+// })
 
 
 
